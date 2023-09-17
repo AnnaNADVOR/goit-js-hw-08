785779c4d0c7030b0a79c38fb4ec6e458946597d
Anna <anjanadvorna@gmail.com>
2023-09-17 20:23:49 +0300

    add 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
+
+player.on('timeupdate', onTimeUpdate);
+function onTimeUpdate(data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+}
+
+const savedTime = localStorage.getItem("videoplayer-current-time");
+console.log('savedTime:',savedTime);
+
+try {
+    const parsedSavedTime = JSON.parse(savedTime);
+    console.log('parsedSavedTime:', parsedSavedTime);
+    player.setCurrentTime(parsedSavedTime.seconds);
+} catch (error) {
+    console.log(error.name);
+    console.log(error.message); 
+}
 
